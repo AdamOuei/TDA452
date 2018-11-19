e44e0965e653a9c3dcfad36ba198afdc736b2510
Omar Oueidat <oueidat@student.chalmers.se>
2018-11-19 10:17:23 +0100

    Clean check functions

--- Labs/Lab3/Sudoku.hs
+++ Labs/Lab3/Sudoku.hs
@@ -44,12 +44,12 @@ allFilledSudoku = Sudoku (replicate 9 (replicate 9 (Just 1)))
 -- | isSudoku sud checks if sud is really a valid representation of a sudoku
 -- puzzle
 isSudoku :: Sudoku -> Bool
-isSudoku (Sudoku rows) = length rows == 9 && checkSudoku rows
+isSudoku (Sudoku rows) = length rows == 9 && checkSudoku rows isDigitOrNothing
 
 -- Checks that each row is of size 9 and calls helpermethod to check that each element is empty or 1-9 
-checkSudoku :: [[Maybe Int]] -> Bool
-checkSudoku [] = True
-checkSudoku (row:rows) = length row == 9 && checkRow row isDigitOrNothing && checkSudoku rows 
+checkSudoku :: [[Maybe Int]] -> (Maybe Int -> Bool) -> Bool
+checkSudoku [] _ = True
+checkSudoku (row:rows) f = length row == 9 && checkRow row f && checkSudoku rows f
 
 -- Checks that each element is either empty or between 1-9
 checkRow :: [Maybe Int] -> (Maybe Int -> Bool) -> Bool
@@ -65,11 +65,7 @@ isDigitOrNothing number = number < Just 10 && number > Just 0
 -- | isFilled sud checks if sud is completely filled in,
 -- i.e. there are no blanks
 isFilled :: Sudoku -> Bool
-isFilled (Sudoku rows) = isSudoku (Sudoku rows) && checkFilledList rows
-
-checkFilledList :: [[Maybe Int]] -> Bool
-checkFilledList [] = True
-checkFilledList (row:rows) = checkRow row isDigit && checkFilledList rows
+isFilled (Sudoku rows) = isSudoku (Sudoku rows) && checkSudoku rows isDigit
 
 isDigit :: Maybe Int -> Bool
 isDigit Nothing = False
